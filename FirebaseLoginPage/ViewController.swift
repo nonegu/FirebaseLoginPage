345dead10ea68be88e8b43ab3c893942859ce5c7
Ender Güzel <enderguzel@gmail.com>
2019-04-03 14:19:01 +0300

    Initial Commit

---
+++ FirebaseLoginPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLoginPage
+//
+//  Created by Ender Güzel on 3.04.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
